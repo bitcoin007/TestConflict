410f70ab8b944b5c3d0d23910ce32c233104a819
Mona Lisa <A241540979@gmail.com[core]>
2018-05-03 20:10:37 +0700

    first commit

---
+++ LibOne/Human.cs
@@ -0,0 +1,9 @@
+﻿namespace LibOne
+{
+    public class Human
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
